5b2714edcf012d44ec1419217d8f18969377d240
Reza <muhammadfahreza@gmail.com>
2025-01-17 16:33:17 +0700

    remove

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,10 +31,10 @@ export const auth = betterAuth({
     }),
   ],
   socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    },
+    // google: {
+      // clientId: process.env.GOOGLE_CLIENT_ID!,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // },
   },
   databaseHooks: {
     user: {
