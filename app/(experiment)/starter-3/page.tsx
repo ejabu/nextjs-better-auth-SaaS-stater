84556880a7dfdd3d60496fc55dab7235ece6adc9
Reza <muhammadfahreza@gmail.com>
2025-02-11 18:10:40 +0400

    rendered twice

---
+++ app/(experiment)/starter-3/page.tsx
@@ -0,0 +1,11 @@
+import Starter3Component from "@/app/(experiment)/starter-3/widget";
+
+const Page = () => {
+	return (
+		<div>
+			<Starter3Component />
+		</div>
+	);
+};
+
+export default Page;
